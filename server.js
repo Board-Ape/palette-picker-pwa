db6652a01f86e1499067400a00c532d1fbc89349
Sam Singer <samuel.singer1@gmail.com>
2018-01-22 23:41:49 -0700

    HTML File Now Displaying

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
-app.get('/', (request, response) => {
-  response.send('Hello We Are Working!');
-});
+// app.get('/', (request, response) => {
+//   response.send('Hello We Are Working!');
+// });
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
