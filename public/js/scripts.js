e806b6652f62849a42f3d727ab3523e4bd7efda3
Sam Singer <samuel.singer1@gmail.com>
2018-01-23 12:25:00 -0700

    Locking Color In Is Consoling Not Locking Though

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,10 +2,26 @@ const randomColorGeneration = () => {
   return "#" + (Math.random() * 0xFFFFFF << 0).toString(16);
 };
 
-$(".random-palette-button").click(() => {
-  $(".one").css("background-color", randomColorGeneration());
-  $(".two").css("background-color", randomColorGeneration());
-  $(".three").css("background-color", randomColorGeneration());
-  $(".four").css("background-color", randomColorGeneration());
-  $(".five").css("background-color", randomColorGeneration());
-});
+const randomizeColors = () => {
+  $(".color1").css("background-color", randomColorGeneration());
+  $(".color2").css("background-color", randomColorGeneration());
+  $(".color3").css("background-color", randomColorGeneration());
+  $(".color4").css("background-color", randomColorGeneration());
+  $(".color5").css("background-color", randomColorGeneration());
+};
+
+const toggleLock = event => {
+  const { id } = event.target;
+
+  if (id.includes('color')) {
+    console.log('lock');
+    $(`#${id}`).attr('id', `lock${id.substr(id.length - 1)}`);
+  } else {
+    console.log('unlock');
+    $(`#${id}`).attr('id', `color${id.substr(id.length - 1)}`);
+  }
+};
+
+$(window).load(() => randomizeColors());
+$(".random-palette-button").click(randomizeColors);
+$(".color").click(event => toggleLock(event));
