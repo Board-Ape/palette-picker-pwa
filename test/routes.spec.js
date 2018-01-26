8064ea1814d25d0f900bb5c2b16ae7cacb8f6bbc
Sam Singer <samuel.singer1@gmail.com>
2018-01-25 23:39:24 -0700

    Testing HTML

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,10 +3,36 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
+  it('should display the homepage correctly', () => {
+    chai.request(server)
+      .get('/')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.html;
+        response.res.text.includes('Palette Picker');
+      })
+      .catch(error => {
+        throw error;
+      });
+  });
 
+  it('should return a 404 if the route does not exist', () => {
+    chai.request(server)
+      .get('/poorRoutePath')
+      .then(response => {
+        response.should.have.status(404);
+      })
+      .catch(error => {
+        throw error;
+      });
+  });
 });
 
 describe('API Routes', () => {
