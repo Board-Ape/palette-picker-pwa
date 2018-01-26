e53c283b514d14c2d51549a7406e10b8b499cf7f
Sam Singer <samuel.singer1@gmail.com>
2018-01-26 00:44:38 -0700

    Testing Post Projects

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -113,6 +113,51 @@ describe('API Routes', () => {
           throw error;
         });
     });
+
+    it('should return status 500 if project does not exist', (done) => {
+      chai.request(server)
+        .get('/api/v1/projects/*/palettes')
+        .then(response => {
+          response.should.have.status(500);
+          done();
+        })
+        .catch(error => {
+          throw error;
+        });
+    });
+  });
+
+  describe('POST /api/v1/projects/', () => {
+    it('should add a new project in the database', (done) => {
+      chai.request(server)
+        .post('/api/v1/projects')
+        .send({
+          id: 2,
+          name: 'People Try'
+        })
+        .then(response => {
+          response.should.have.status(201);
+          response.body.should.be.a('array');
+          response.body.length.should.equal(1);
+          response.body[0].should.have.property('id');
+          response.body[0].id.should.equal(2);
+          response.body[0].should.have.property('name');
+          response.body[0].name.should.equal('People Try');
+
+          chai.request(server)
+            .get('/api/v1/projects')
+            .then(response => {
+              response.should.have.status(200);
+              response.should.be.json;
+              response.body.should.be.a('array');
+              response.body.length.should.equal(2);
+              done();
+            })
+            .catch(error => {
+              throw error;
+            });
+        });
+    });
   });
 
 });
