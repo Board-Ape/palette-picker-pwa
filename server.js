28ff481382a2c6582f8cf7530c5afc3519dfc8e1
Sam Singer <samuel.singer1@gmail.com>
2018-01-30 10:18:30 -0700

    Initial Commit AWS

--- server.js
+++ server.js
@@ -77,6 +77,17 @@ app.get('/api/v1/projects/:id/palettes', (request, response) => {
     });
 });
 
+app.get('/api/v1/projects/:id', (request, response) => {
+  database('projects')
+    .where({ project_id: request.params.id}).select()
+    .then(projects => {
+      response.status(200).json(projects);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
 app.delete('/api/v1/palettes/:id', (request, response) => {
   const id = request.params;
   database('palettes').where(id).del()
@@ -90,24 +101,10 @@ app.delete('/api/v1/palettes/:id', (request, response) => {
     .catch(error => response.status(500).json({ error }));
 });
 
-// app.post('/api/v1/palettes', (request, response) => {
-//   const palette = request.body;
-//   for (let requiredParameter of ['name', 'hex1', 'hex2', 'hex3', 'hex4', 'hex5']) {
-//     if (!palette[requiredParameter]) {
-//       return response.status(422).send({ error: `You're missing a ${requiredParameter}.` });
-//     }
-//   }
-//   database('palettes').insert(palette, '*')
-//     .then(palette => {
-//       response.status(201).json(palette);
-//     })
-//     .catch(error => {
-//       response.status(500).json({ error });
-//     });
-// });
+
 app.delete('/api/v1/projects/:id', (request, response) => {
   const id = request.params;
-  database('projects').where(id).del()
+  database('projects').where(project_id).del()
     .then(result => {
       if (!result) {
         response.status(422).json({ error: 'no project' });
@@ -117,23 +114,6 @@ app.delete('/api/v1/projects/:id', (request, response) => {
     })
     .catch(error => response.status(500).json({ error }));
 });
-
-// app.post('/api/v1/projects', (request, response) => {
-//   const project = request.body;
-//   for (let requiredParameter of ['name']) {
-//     if (!project[requiredParameter]) {
-//       return response.status(422).send({ error: `You're missing a ${requiredParameter}.` });
-//     }
-//   }
-//   database('projects').insert(project, '*')
-//     .then(project => {
-//       response.status(201).json(project);
-//     })
-//     .catch(error => {
-//       response.status(500).json({ error });
-//     });
-// });
-
 
 app.listen(app.get('port'), () => {
   console.log(`running on ${app.get('port')}.`);
